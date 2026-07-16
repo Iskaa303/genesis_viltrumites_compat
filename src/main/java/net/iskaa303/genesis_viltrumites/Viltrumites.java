cbcb2c419252ea290d1af8501837f73b5b6fcb01
Iskander 'Iskaa303' Madikhan <iska060808@gmail.com>
2026-07-16 19:12:42 -0400

    add immersive portals and fix dimension stack

--- src/main/java/net/iskaa303/genesis_viltrumites/Viltrumites.java
+++ src/main/java/net/iskaa303/genesis_viltrumites/Viltrumites.java
@@ -5,8 +5,12 @@ import net.iskaa303.genesis_viltrumites.config.CompatConfig;
 import net.iskaa303.genesis_viltrumites.event.SpaceTransitionHandler;
 import net.iskaa303.genesis_viltrumites.viltrumite.PerPlayerSpeedManager;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.level.chunk.LevelChunk;
 import net.minecraftforge.event.RegisterCommandsEvent;
+import net.minecraftforge.event.level.ChunkEvent;
 import net.minecraftforge.fml.common.Mod;
+import qouteall.imm_ptl.peripheral.dim_stack.DimStackManagement;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,10 +24,18 @@ public final class Viltrumites {
         PerPlayerSpeedManager.init();
         MinecraftForge.EVENT_BUS.register(new SpaceTransitionHandler());
         MinecraftForge.EVENT_BUS.addListener(this::onRegisterCommands);
+        MinecraftForge.EVENT_BUS.addListener(this::onChunkLoad);
         LOGGER.info("Genesis: Viltrumites loaded.");
     }
 
     private void onRegisterCommands(RegisterCommandsEvent event) {
         FlightSpeedCommand.register(event.getDispatcher());
+    }
+
+    private void onChunkLoad(ChunkEvent.Load event) {
+        if (event.getLevel() instanceof ServerLevel serverLevel
+            && event.getChunk() instanceof LevelChunk levelChunk) {
+            DimStackManagement.replaceBedrock(serverLevel, levelChunk);
+        }
     }
 }
